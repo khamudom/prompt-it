331c5308fd75d99ffd159ce2c7ff250299bd40ed
khamudom <khamu@outlook.com>
2023-08-05 07:39:12 -0700

    ignore env file

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -9,16 +9,20 @@ const PromptCard = ({ post, handleTagClick, handleDelete, handleEdit }) => {
   const { data: session } = useSession();
 
   return (
-    <div className="prompt-card">
+    <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
         <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
           <Image
-            src={session.user.image}
+            src={session?.user?.image}
             alt="user image"
             width={40}
             height={40}
             className="rounded-full object-contain"
           />
+          <div className="flex flex-col">
+            <h3>{post.creator?.username}</h3>
+            <p>{post.creator?.email}</p>
+          </div>
         </div>
       </div>
     </div>
